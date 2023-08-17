d6ab787dcb7e1cb01548876c756835adcde23370
rioribeirods <rioribeirods@gmail.com>
2023-08-17 16:09:07 -0300

    program3.7.c updated

--- exercices/section3/program3.7.c
+++ exercices/section3/program3.7.c
@@ -2,9 +2,9 @@
 
 int main(){
     float num;
-    printf("choose your celsius temperature: ");
+    printf("choose your fahrenheit temperature: ");
     scanf("%f", &num);
     num = 5.0 * (num - 32.0)/9.0;
-    printf("the temperature you chose in fahrenheit is: %.3f", num);
+    printf("the temperature you chose in celsius is: %.3f", num);
     return 0;
 }
